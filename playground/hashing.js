99cb4c4d4c0163efb1b87f19c43bde18e8df0749
webFarrock <webFarrock@mail.ru>
2017-01-27 14:46:10 +0300

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,11 +1,28 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+let password = '123asdf';
+
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log('hash: ',hash);
+    });
+});
+
+
+
+let hp = '$2a$10$n5A9Ge12vkJiZu/IqTNAU0e4fCLyK8CVyiS3mw9Sa.mdBQoElx93u6';
+
+bcrypt.compare(password, hp, (err, res) => {
+    if(res) console.log('++++');
+    if(!res) console.log('---');
+});
+
+/*
 let data = {
     id: 10
 };
-
-
 
 let token = jwt.sign(data, 'secretcode');
 
@@ -13,7 +30,7 @@ console.log('token, ', token);
 
 let decoded = jwt.verify(token, 'secretcode');
 console.log('decoded: ', decoded);
-
+*/
 /*
 let message = 'I am user number 3';
 let hash = SHA256(message).toString();

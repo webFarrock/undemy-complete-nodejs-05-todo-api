058ae05c8d02e0b098ecab4b021de0228a7215c4
webFarrock <webFarrock@mail.ru>
2017-01-31 11:42:32 +0300

    add test for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -11,12 +11,6 @@ const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 beforeEach(populateUsers);
 beforeEach(populateTodos);
 
-/*
-beforeEach((done) => {
-    Todo.remove({}).then(() => done());
-});
-*/
-
 
 describe('POST /todos', () => {
 
@@ -319,4 +313,21 @@ describe('POST /users/login', () => {
             });
     });
 
+});
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if(err) return done(err);
+
+                User.findById(users[0]._id).then(user => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch(e => done(e));
+            });
+    });
 });
